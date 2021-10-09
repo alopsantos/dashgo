d258dab06466f9aafb62281e055d9b0e2279c184
Anderson Lopes <anderson_lops@hotmail.com>
2021-10-08 22:27:14 -0300

    Component Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Button, Stack } from "@chakra-ui/react";
 
-export default function Home() {
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
-      <h1>Dashgo</h1>
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+
+          <Input name="password" type="password" label="Senha" />
+
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
     </Flex>
-  )
+  );
 }
